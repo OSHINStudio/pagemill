6512154b00e47b2143e0257ff455d6be0e522d3a
Fred Snyder <fred@blindacre.com>
2012-11-04 13:38:01 -0500

    Select and option tags.

--- classes/Pagemill/Attribute/Selected.php
+++ classes/Pagemill/Attribute/Selected.php
@@ -1,8 +1,12 @@
 <?php
 class Pagemill_Attribute_Selected extends Pagemill_Attribute_Hidden {
 	public function __construct($name, $value, Pagemill_Tag $tag) {
+		if (!is_a($tag, 'Pagemill_Tag_Html_Select')) {
+			throw new Exception("The {$name} attribute is only valid on select elements " . get_class($tag));
+		}
 		parent::__construct($name, $value, $tag);
-		$selectvalue = new Pagemill_TagPreprocessor_SelectValue($value);
+		$tag->attachPreprocess(new Pagemill_TagPreprocessor_SetSelectValue($value));
+		$selectvalue = new Pagemill_TagPreprocessor_SelectValue($tag);
 		$this->_attachToOptions($tag, $selectvalue);
 	}
 	private function _attachToOptions(Pagemill_Tag $parent, Pagemill_TagPreprocessor_SelectValue $selectvalue) {
