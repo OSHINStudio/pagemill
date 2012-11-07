3ac68569300ccc570373787fb5f3f6663fbe1c0c
Fred Snyder <fsnyder@castwide.com>
2012-11-07 08:52:19 -0500

    Bug in function declaration.

--- classes/Pagemill/TagPreprocessor/CheckValue.php
+++ classes/Pagemill/TagPreprocessor/CheckValue.php
@@ -5,7 +5,7 @@ class Pagemill_TagPreprocessor_CheckValue extends Pagemill_TagPreprocessor {
 	public function __construct($checkvalue) {
 		$this->_checkvalue = $checkvalue;
 	}
-	public function process(Pagemill_Tag $tag, Pagemill_Data $data) {
+	public function process(Pagemill_Tag $tag, Pagemill_Data $data, Pagemill_Stream $stream) {
 		if ($tag->hasAttribute('value')) {
 			$checked = $data->parseVariables($this->_checkvalue);
 			$value = $data->parseVariables($tag->getAttribute('value'));
