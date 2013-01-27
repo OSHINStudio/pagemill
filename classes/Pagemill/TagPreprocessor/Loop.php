3dc624d7a1251a01a235e9b2c6f67008cab8f234
Fred Snyder <fsnyder@castwide.com>
2013-01-27 15:24:58 -0500

    pm:loop attribute rewrites tag tree for proper data scope and iteration.

--- classes/Pagemill/TagPreprocessor/Loop.php
+++ classes/Pagemill/TagPreprocessor/Loop.php
@@ -11,12 +11,15 @@ class Pagemill_TagPreprocessor_Loop extends Pagemill_TagPreprocessor {
 		$attributes['name'] = $parts[0];
 		if (isset($parts[1])) $attributes['as'] = $parts[1];
 		$loop = new Pagemill_Tag_Loop('loop', $attributes, null, $tag->doctype());
-		$parent = $tag->parent();
-		$parent->appendChild($loop);
-		$loop->appendChild($tag);
+		$temptag = new Pagemill_Tag($tag->name(), $tag->attributes(), null, $tag->doctype());
+		foreach ($tag->children() as $child) {
+			$temptag->appendChild($child);
+		}
+		$loop->appendChild($temptag);
 		$loop->process($data, $stream);
-		$parent->appendChild($tag);
-		$loop->detach();
+		foreach ($temptag->children() as $child) {
+			$tag->appendChild($child);
+		}
 		return false;
 	}
 }
