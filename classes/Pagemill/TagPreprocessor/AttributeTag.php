09a0e10d8229c73baf213c2acdf5341ca0e2e769
Fred Snyder <fred@blindacre.com>
2014-02-07 08:57:01 -0500

    Trim attribute values.

--- classes/Pagemill/TagPreprocessor/AttributeTag.php
+++ classes/Pagemill/TagPreprocessor/AttributeTag.php
@@ -10,7 +10,7 @@ class Pagemill_TagPreprocessor_AttributeTag extends Pagemill_TagPreprocessor {
 		$value = $stream->peek();
 		if ($value !== '') {
 			$value = html_entity_decode($value);
-			$tag->setAttribute($this->_attributeTag->getAttribute('name'), $value);
+			$tag->setAttribute($this->_attributeTag->getAttribute('name'), trim($value));
 		}
 	}
 }
